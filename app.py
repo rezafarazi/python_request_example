e57bd8e41bd4c4edc682ef859b1f4c933634434e
reza farazi <rezafarazi1378@gmail.com>
2022-09-26 19:30:03 +0330

    Add Catch

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #Libraries Start
+from tokenize import String
 import requests
 import json
 #Libraries End
@@ -12,10 +13,19 @@ def GetData(js):
 #Get Use Json Function End
 
 
+#Catch Data Start
+def Catch(data):
+    with open("C:/Users/Rezafta/Desktop/python_request_test/catch.txt","w") as catch:
+        catch.write("data")
+        catch.close()
+#Catch Data End
+
+
 #Main Function Start
 if __name__=="__main__":
     req = requests.get("https://rezafarazi.github.io/Online_Json_Api/sorapp_posts.json")
     all_text=req.text
+    Catch(all_text)
     GetData(all_text)
 #Main Function End
 
