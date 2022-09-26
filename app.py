7a414f11d43e77f7fbad6fad4280ee2499291383
reza farazi <rezafarazi1378@gmail.com>
2022-09-26 19:19:01 +0330

    Use Json

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 #Libraries Start
 import requests
+import json
 #Libraries End
+
+
+#Get Use Json Function Start
+def GetData(js):
+    data=json.loads(js)
+    for item in data:
+        print("id is ", item["Id"], " Title Is ",item["Title"]," Image Address is : ",item["Image"])
+#Get Use Json Function End
 
 
 #Main Function Start
 if __name__=="__main__":
     req = requests.get("https://rezafarazi.github.io/Online_Json_Api/sorapp_posts.json")
-    print(req.text)
+    all_text=req.text
+    GetData(all_text)
 #Main Function End
+
+
